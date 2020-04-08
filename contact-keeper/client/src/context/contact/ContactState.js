43e023a27f57c4ddeb78f75da40ca1dcf05a15e9
AlexandreGuerra <agmguerra@gmail.com>
2020-04-08 01:02:07 -0300

    add contact function

--- contact-keeper/client/src/context/contact/ContactState.js
+++ contact-keeper/client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid }  from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 
@@ -43,6 +43,13 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid();
+    dispatch({ 
+      type: ADD_CONTACT, 
+      payload: contact 
+    });
+  }
 
   // Delete Contact
 
@@ -59,7 +66,8 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
     value={{
-      contacts: state.contacts
+      contacts: state.contacts,
+      addContact
     }}>
       { props.children }
     </ContactContext.Provider>
