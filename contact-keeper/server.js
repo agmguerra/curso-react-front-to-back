d130e3cac9e66f121379254b20120f741494f52a
AlexandreGuerra <agmguerra@gmail.com>
2020-04-11 22:16:11 -0300

    ajustes para deploy

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -15,6 +16,14 @@ app.get('/', (req, res) => res.send({ msg: 'Welcome to the ContactKeeper API...'
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('contact-keeper/client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000
 
