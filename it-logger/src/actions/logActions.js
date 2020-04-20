d84ce1ef50326bb608acd517b1c103fdfa80506c
AlexandreGuerra <agmguerra@gmail.com>
2020-04-19 21:15:46 -0300

    consulta e inclusao de log através de react redux

---
+++ it-logger/src/actions/logActions.js
@@ -0,0 +1,77 @@
+import { 
+ GET_LOGS,
+ ADD_LOG,
+ SET_LOADING,
+ LOGS_ERROR
+} from './types';
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  }
+}
+
+// Get logs from server
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+    
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+
+}
+
+// Add new log from server
+export const addLog = (log) => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+    
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+
+}
+
+
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data
+//     });
+//   }
+// }
+
